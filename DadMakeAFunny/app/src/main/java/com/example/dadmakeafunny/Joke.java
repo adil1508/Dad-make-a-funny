4116b20bba4115a4221bcd81ec3663e2976506bc
Asil Mian <asilmian@gmail.com>
2019-03-27 13:52:15 +1100

    half implementation of JokeReader and Jokes\n Not functioning but decent desgin code

---
+++ DadMakeAFunny/app/src/main/java/com/example/dadmakeafunny/Joke.java
@@ -0,0 +1,20 @@
+package com.example.dadmakeafunny;
+
+/*
+Joke class containing all the jokes as strings
+ */
+
+public class Joke {
+    public String title;
+    public String text;
+
+    public Joke(String title, String text, String after){
+        this.title = title;
+        this.text = text;
+    }
+
+    public Joke(){
+        this.title = null;
+        this.text = null;
+    }
+}

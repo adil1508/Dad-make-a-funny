52087cac7e03bac586002d957c87c82de42dcb4b
Asil Mian <asilmian@gmail.com>
2019-03-29 12:37:44 +1100

    added getters and setters to joke + readConnection function for JokesReader

--- DadMakeAFunny/app/src/main/java/com/example/dadmakeafunny/Joke.java
+++ DadMakeAFunny/app/src/main/java/com/example/dadmakeafunny/Joke.java
@@ -5,16 +5,43 @@ Joke class containing all the jokes as strings
  */
 
 public class Joke {
-    public String title;
-    public String text;
 
-    public Joke(String title, String text){
-        this.title = title;
-        this.text = text;
-    }
+    private String title;
+    private String text;
+    private String link;
 
     public Joke(){
         this.title = null;
         this.text = null;
+        this.link = null;
     }
+
+    //getters and setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+
+
 }
--- DadMakeAFunny/app/src/main/java/com/example/dadmakeafunny/JokesReader.java
+++ DadMakeAFunny/app/src/main/java/com/example/dadmakeafunny/JokesReader.java
@@ -1,6 +1,14 @@
 package com.example.dadmakeafunny;
 import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -8,15 +16,16 @@ import java.util.ArrayList;
 public class JokesReader {
 /* class opens connection to reddit and handles all jokes */
     private String url;
-    public ArrayList<Joke> jokes;
-    private String after;
+    private ArrayList<Joke> jokes;
+    private String last_joke_id;
     private static final String BASE_URL = "https://www.reddit.com/r/dadjokes/.json";
-    private String limit = "?limit=20";
+    private static final String LIMIT_FLAG  = "?limit=";
+    private int limit = 20;
 
     /*constructor*/
     public JokesReader(){
         this.jokes = new ArrayList<>();
-        this.after = null;
+        this.last_joke_id = null;
     }
 
 
@@ -25,10 +34,12 @@ public class JokesReader {
     public void startConnection(){
         this.url = createUrl();
         InputStream connection = this.sendGet(url);
-        this.jokes = readConnection(connection);
+        String data = this.readConnection(connection);
+        this.pullJokes(data);
     }
 
 
+    //returns an InputStream from the URL
     private InputStream sendGet(String url){
         InputStream connection = null;
         try {
@@ -42,22 +53,41 @@ public class JokesReader {
 
     //create a url based on if accessing first time or new access
     private String createUrl(){
-        if (this.after == null){
-            return this.BASE_URL + this.limit;
+        if (this.last_joke_id == null){
+            return (this.BASE_URL + this.LIMIT_FLAG + this.limit);
         }
         else{
-            return this.BASE_URL + "?after=" + this.after + this.limit;
+            return (this.BASE_URL + "?after=" + this.last_joke_id + this.LIMIT_FLAG + this.limit);
         }
     }
 
-    private ArrayList<Joke> readConnection(InputStream connection){
-        //still left to implement
-        return null;
+    //return contents of connection as a string
+    private String readConnection(InputStream connection){
+        String result = "";
+        String tmp;
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection));
+        try {
+            while( (tmp = br.readLine()) != null){
+                result += tmp + "\n";
+            }
+            br.close();
+            return result;
+        }catch (IOException e){
+            Log.d("readConnection()", e.toString());
+            return null;
+        }
     }
 
+    private void pullJokes(String data) {
+        return;
+    }
 
     public ArrayList<Joke> refreshJokes(){
         //will get more jokes, after the current ones
+    }
+
+    public ArrayList<Joke> getJokes(){
+        return this.jokes;
     }
 
     /*functions left to write

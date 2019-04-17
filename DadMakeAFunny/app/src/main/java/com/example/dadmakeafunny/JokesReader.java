bf2a7c8c42ae52d0cdaf6ce19cbc764ffcc9d1c9
Asil Mian <asilmian@gmail.com>
2019-04-17 16:55:39 +1000

    removed redundant code

--- DadMakeAFunny/app/src/main/java/com/example/dadmakeafunny/JokesReader.java
+++ DadMakeAFunny/app/src/main/java/com/example/dadmakeafunny/JokesReader.java
@@ -12,11 +12,9 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 
 
 public class JokesReader {
